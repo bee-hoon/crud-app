c949a4a640e6c794a64b6fd86ff138274bea7fb1
behoon <syauqeesy@gmail.com>
2023-02-25 21:38:19 +0700

    chore: Setup web server & dockerized server service

---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express'
+import config from './config'
+
+const app = express()
+
+app.all('*', (_: Request, response: Response): void => {
+  response.status(404).json({
+    status: false,
+    message: 'service not supported',
+    data: null
+  })
+})
+
+app.listen(config.port, () => console.log(`server started on port ${config.port}`))
