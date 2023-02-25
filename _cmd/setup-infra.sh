fadfa910e7d69119c7c314b5edb6d90839ff0112
behoon <syauqeesy@gmail.com>
2023-02-25 11:30:54 +0700

    chore: Create setup infra script

---
+++ _cmd/setup-infra.sh
@@ -0,0 +1,15 @@
+cd _infrastructure
+
+while getopts t: flag
+do
+    case "${flag}" in
+        t) exec_type=${OPTARG};;
+    esac
+done
+
+if [ $exec_type = 'up' ]
+then
+    docker-compose up -d
+else
+    docker-compose down
+fi
