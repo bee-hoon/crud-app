3c262f8344969e44bf48c695f39dfeb5e85fc1ae
behoon <syauqeesy@gmail.com>
2023-02-25 11:43:47 +0700

    style: 2 spaces indentation

--- _cmd/setup-infra.sh
+++ _cmd/setup-infra.sh
@@ -2,14 +2,14 @@ cd _infrastructure
 
 while getopts t: flag
 do
-    case "${flag}" in
-        t) exec_type=${OPTARG};;
-    esac
+  case "${flag}" in
+  	t) exec_type=${OPTARG};;
+  esac
 done
 
 if [ $exec_type = 'up' ]
 then
-    docker-compose up -d
+  docker-compose up -d
 else
-    docker-compose down
+  docker-compose down
 fi
