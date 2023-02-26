2719d9e7e676e65640ca9a959b51902ffe3e188a
behoon <syauqeesy@gmail.com>
2023-02-26 09:38:31 +0700

    style: Use prettier

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,16 @@
-import express, { Request, Response } from 'express'
-import config from './config'
+import express, { Request, Response } from "express";
+import config from "./core/config";
 
-const app = express()
+const app = express();
 
-app.all('*', (_: Request, response: Response): void => {
+app.all("*", (_: Request, response: Response): void => {
   response.status(404).json({
     status: false,
-    message: 'service not supported',
-    data: null
-  })
-})
+    message: "service not supported",
+    data: null,
+  });
+});
 
-app.listen(config.port, () => console.log(`server started on port ${config.port}`))
+app.listen(config.port, () =>
+  console.log(`server started on port ${config.port}`)
+);
