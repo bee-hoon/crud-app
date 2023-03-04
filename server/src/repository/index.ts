0be04b588766c0e4473c94e6ea05495c9ff9eac5
behoon <syauqeesy@gmail.com>
2023-03-04 12:41:14 +0700

    chore: create init repository

---
+++ server/src/repository/index.ts
@@ -0,0 +1,17 @@
+import { DataSource } from "typeorm";
+
+import ProductEntity from "../entity/Product";
+
+async function initRepository(dataSource: DataSource) {
+  const connection = await dataSource.initialize();
+  const result = await connection.query("SELECT 1 + 1");
+
+  console.log(result);
+  console.log("data source initialized");
+
+  return {
+    Product: connection.getRepository(ProductEntity),
+  };
+}
+
+export default initRepository;
